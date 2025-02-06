c2017a0a941b15e411461a84328f1458a995d1c4
TechnologicNick <technologicnick@gmail.com>
2025-02-06 22:40:54 +0100

    Fix Lua API function names with unanalyzed string names

--- find_lua_api_functions.py
+++ find_lua_api_functions.py
@@ -33,6 +33,16 @@ if not bv.get_tag_type("Lua API"):
 #   lua_CFunction func;
 # } luaL_Reg;
 
+def assert_string_reference_at(address: int) -> StringReference:
+    """Get the string reference at the given address, or create a new one if it doesn't exist."""
+
+    funcname = bv.get_string_at(address)
+    if not funcname:
+        funcname = bv.get_ascii_string_at(address, min_length=0)
+        bv.define_user_data_var(address, Type.array(Type.char(), funcname.length))
+
+    return funcname
+
 for ref in register_var.code_refs:
     print(ref.mlil)
     [_, libname, reg_ptr] = [x[1] for x in ref.mlil.detailed_operands if x[0] == "params"][0]
@@ -40,7 +50,7 @@ for ref in register_var.code_refs:
     if not isinstance(libname, MediumLevelILConstPtr):
         continue
 
-    libname = bv.get_string_at(cast(MediumLevelILConstPtr, libname).constant)
+    libname = assert_string_reference_at(cast(MediumLevelILConstPtr, libname).constant)
     reg_ptr = cast(MediumLevelILConstPtr, reg_ptr).constant
 
     i = 0
@@ -49,7 +59,8 @@ for ref in register_var.code_refs:
         if not reg:
             break
 
-        funcname = bv.get_string_at(reg)
+        funcname = assert_string_reference_at(reg)
+
         func = bv.get_function_at(bv.read_pointer(reg_ptr + i * 0x10 + 8))
 
         print(funcname, func)
