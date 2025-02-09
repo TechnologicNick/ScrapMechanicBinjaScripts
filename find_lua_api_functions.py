e1379934916f189a540b47d4ef6af49c60bb1f10
TechnologicNick <technologicnick@gmail.com>
2025-02-09 05:09:36 +0100

    Add finding Lua class registrations through function

--- find_lua_api_functions.py
+++ find_lua_api_functions.py
@@ -3,6 +3,29 @@ from typing import cast
 
 bv = cast(BinaryView, bv) # type: ignore
 
+sm_types = bv.parse_types_from_string("""
+                                      
+struct LuaClassDefinition __packed
+{
+    char const* m_pClassName;
+    uint32_t m_uTypeId;
+    uint32_t m_uDomain;
+    void* m_pCallbackToString;
+    void* m_pCallbackSomeBoolean;
+    void* m_pCallbackSerialize;
+    void* m_pCallbackDeserialize;
+};
+                                      
+""")
+
+for name in sm_types.types:
+    bv.define_user_type(name, sm_types.types[name])
+
+LuaClassDefinition = cast(StructureType, bv.get_type_by_name("LuaClassDefinition"))
+
+def get_member(struct: StructureType, member_name: str) -> StructureMember:
+    return next(cast(StructureMember, mem) for mem in struct.members if mem.name == member_name)
+
 if not bv.get_tag_type("Lua API"):
     bv.create_tag_type("Lua API", "🌌")
 
@@ -79,15 +102,17 @@ def find_static_functions() -> None:
 def register_class(name: str, typeid: int, metatable_ptr: int, member_functions_ptr: int) -> None:
     print(name, typeid, hex(metatable_ptr), hex(member_functions_ptr))
 
-    rename_lua_functions(name + ":", metatable_ptr)
-    rename_lua_functions(name + ":", member_functions_ptr)
+    if metatable_ptr:
+        rename_lua_functions(name + ":", metatable_ptr)
+    if member_functions_ptr:
+        rename_lua_functions(name + ":", member_functions_ptr)
 
 def read_const_char_ptr_argument(hlil: HighLevelILInstruction, index: int) -> Optional[Tuple[StringReference, Optional[int]]]:
     if not isinstance(hlil, HighLevelILCall):
         raise Exception(f"Reference at {hex(hlil.address)} is not a call")
     
     param = hlil.params[1]
-    if isinstance(param, HighLevelILDeref):
+    if isinstance(param, HighLevelILDeref) or isinstance(param, HighLevelILDerefField):
         if isinstance(param.src, HighLevelILConstPtr):
             return (assert_string_reference_at(bv.read_pointer(param.src.constant)), param.src.constant)
         elif isinstance(param.src, HighLevelILVar):
@@ -113,6 +138,11 @@ def assert_hlil_calls_function(hlil_function: HighLevelILInstruction, func: Core
         raise Exception(f"Reference at {hex(hlil_function.address)} is not a call to {func.name}")
     
     return hlil_function
+
+def update_function_name(func: Function, new_function_name: str) -> str:
+    combined = "::".join(func.name.split("::")[0:-1] + [new_function_name])
+    func.name = combined
+    return combined
 
 def find_classes() -> None:
     [luaL_newmetatable] = bv.get_symbols_by_name(
@@ -140,6 +170,8 @@ def find_classes() -> None:
         True,
     )
 
+    class_registration_refs: set[ReferenceSource] = set()
+
     for ref in newmetatable_var.code_refs:
         hlil_newmetatable = ref.hlil
         if not hlil_newmetatable:
@@ -147,6 +179,7 @@ def find_classes() -> None:
         
         name = read_const_char_ptr_argument(hlil_newmetatable, 1)
         if not name:
+            class_registration_refs.add(ref)
             continue
 
         (name, name_ptr) = name
@@ -157,9 +190,8 @@ def find_classes() -> None:
         else:
             new_function_name = f"RegisterClass_{name.value}"
         func = ref.function
-        new_function_name = "::".join(func.name.split("::")[0:-1] + [new_function_name])
+        new_function_name = update_function_name(func, new_function_name)
         print(hex(func.start), new_function_name)
-        func.name = new_function_name
 
         # Not a class registration function
         if name.value == "weak":
@@ -183,8 +215,8 @@ def find_classes() -> None:
         assert isinstance(param_typeid, HighLevelILSx), f"Reference at {hex(hlil_pushinteger.address)} is not a sign extend"
         assert isinstance(param_typeid.src, HighLevelILDeref), f"Reference at {hex(hlil_pushinteger.address)} is not a deref"
         assert isinstance(param_typeid.src.src, HighLevelILConstPtr), f"Reference at {hex(hlil_pushinteger.address)} is not a const ptr"
-        typeid_var = bv.get_data_var_at(param_typeid.src.src.constant)
-        typeid_var.name = f"LuaAPI::Class::{name.value}::typeid_{typeid_var.value}"
+        typeid = bv.get_data_var_at(param_typeid.src.src.constant)
+        typeid.name = f"LuaAPI::Class::{name.value}::TypeId_{typeid.value}"
 
         hlil_register_metatable = assert_hlil_calls_function(instructions[hlil_pushinteger.instr_index + 2], luaL_register)
 
@@ -194,7 +226,7 @@ def find_classes() -> None:
         assert isinstance(hlil_register_metatable.params[2], HighLevelILConstPtr), f"Reference at {hex(hlil_register_metatable.address)} is not a const ptr"
         metatable_ptr = hlil_register_metatable.params[2].constant
         metatable_ptr_var = bv.get_data_var_at(metatable_ptr)
-        metatable_ptr_var.name = f"ps_LuaAPI::Class::{name.value}::metatable"
+        metatable_ptr_var.name = f"p_LuaAPI::Class::{name.value}::Metatable"
 
         hlil_register_member_functions = assert_hlil_calls_function(instructions[hlil_register_metatable.instr_index + 1], luaL_register)
 
@@ -204,11 +236,63 @@ def find_classes() -> None:
         assert isinstance(hlil_register_member_functions.params[2], HighLevelILConstPtr), f"Reference at {hex(hlil_register_member_functions.address)} is not a const ptr"
         member_functions_ptr = hlil_register_member_functions.params[2].constant
         member_functions_ptr_var = bv.get_data_var_at(member_functions_ptr)
-        member_functions_ptr_var.name = f"ps_LuaAPI::Class::{name.value}::member_functions"
+        member_functions_ptr_var.name = f"p_LuaAPI::Class::{name.value}::MemberFunctions"
 
 
-        register_class(name.value, typeid_var.value, metatable_ptr, member_functions_ptr)
+        register_class(name.value, typeid.value, metatable_ptr, member_functions_ptr)
 
+    assert len(class_registration_refs) == 1, f"Expected 1 class registration function references, got {len(class_registration_refs)}"
+    for ref in class_registration_refs:
+        print("Found class registration function at", ref)
+        func = ref.function
+        assert func, "Reference has no function"
 
-find_static_functions()
+        update_function_name(ref.function, "RegisterClass")
+
+        func.parameter_vars[0].name = "L"
+        func.parameter_vars[1].type = Type.pointer(bv.arch, LuaClassDefinition)
+        func.parameter_vars[1].name = "pDefinition"
+        func.parameter_vars[2].name = "pMetatable"
+        func.parameter_vars[3].name = "pMemberFunctions"
+
+        for ref in DataVariable(bv, func.start, Type.pointer(bv.arch, Type.void()), True).code_refs:
+            print(ref.hlil)
+            assert isinstance(ref.hlil, HighLevelILCall), f"Reference at {hex(ref.address)} is not a call"
+            
+            assert isinstance(ref.hlil.params[1], HighLevelILConstPtr), f"Reference at {hex(ref.address)} is not a const ptr: {ref.hlil.params[1]} ({type(ref.hlil.params[1])})"
+            lua_class_definition_addr = ref.hlil.params[1].constant
+            lua_class_definition = bv.get_data_var_at(lua_class_definition_addr)
+            lua_class_definition.type = LuaClassDefinition
+
+            class_name_addr = bv.read_pointer(lua_class_definition_addr)
+            class_name = assert_string_reference_at(class_name_addr)
+            print(LuaClassDefinition, class_name)
+            bv.get_data_var_at(class_name_addr).name = f"s_LuaAPI::Class::{class_name.value}"
+            lua_class_definition.name = f"LuaAPI::Class::{class_name}::Definition"
+
+            member = get_member(LuaClassDefinition, "m_uTypeId")
+            typeid = bv.read_int(lua_class_definition_addr + member.offset, member.type.width)
+
+            if isinstance(ref.hlil.params[2], HighLevelILConst):
+                assert ref.hlil.params[2].constant == 0, f"Reference at {hex(ref.address)} is not a null pointer: {ref.hlil.params[2].constant}"
+                metatable_ptr = 0
+            else:
+                assert isinstance(ref.hlil.params[2], HighLevelILConstPtr), f"Reference at {hex(ref.address)} is not a const ptr: {ref.hlil.params[2]} ({type(ref.hlil.params[2])})"
+                metatable_ptr = ref.hlil.params[2].constant
+                metatable_var = bv.get_data_var_at(metatable_ptr)
+                metatable_var.name = f"p_LuaAPI::Class::{class_name}::Metatable"
+
+            if isinstance(ref.hlil.params[3], HighLevelILConst):
+                assert ref.hlil.params[3].constant == 0, f"Reference at {hex(ref.address)} is not a null pointer: {ref.hlil.params[3].constant}"
+                member_functions_ptr = 0
+            else:
+                assert isinstance(ref.hlil.params[3], HighLevelILConstPtr), f"Reference at {hex(ref.address)} is not a const ptr: {ref.hlil.params[3]} ({type(ref.hlil.params[3])})"
+                member_functions_ptr = ref.hlil.params[3].constant
+                member_functions_var = bv.get_data_var_at(member_functions_ptr)
+                member_functions_var.name = f"p_LuaAPI::Class::{class_name}::MemberFunctions"
+
+            register_class(class_name.value, typeid, metatable_ptr, member_functions_ptr)
+            
+
+# find_static_functions()
 find_classes()
